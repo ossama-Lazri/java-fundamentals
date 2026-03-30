e6784e0100c1507a1ccbc7e39d9382a45ad1f6d3
Ossama LAZRI <lazriossama18@gmail.com>
2026-03-30 19:34:36 +0100

    Day 1: Java basics - variables, types, operators (5 programs)

---
+++ LogicalExample.java
@@ -0,0 +1,10 @@
+public class LogicalExample {
+    public static void main(String[] args) {
+        boolean isAdult = true;
+        boolean hasTicket = false;
+
+        System.out.println(isAdult && hasTicket);
+        System.out.println(isAdult || hasTicket);
+        System.out.println(!isAdult);
+    }
+}
---
+++ VariablesExample.java
@@ -0,0 +1,13 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        double price = 99.99;
+        String name = "Ossama";
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Student: " + isStudent);
+    }
+}
